a19c91641fc0f3bd7a82f73e19498816a9bb3661
09370190 <emre_karatas@kocsistem.com.tr>
2020-10-12 18:25:08 +0300

    minor fixes utf8

--- lib/networking/generic_request_object.dart
+++ lib/networking/generic_request_object.dart
@@ -19,7 +19,7 @@ import 'request_id.dart';
 import 'serializable.dart';
 import 'serializable_list.dart';
 import 'serializable_object.dart';
-//TODO v
+
 class GenericRequestObject<RequestType extends Serializable, ResponseType extends Serializable, ErrorType extends Serializable> {
   Set<Header> _headers;
   ContentType _contentType;
@@ -258,7 +258,9 @@ class GenericRequestObject<RequestType extends Serializable, ResponseType extend
       );
 
       var buffer = new StringBuffer();
-      var bytes = await consolidateHttpClientResponseBytes(response);
+      await for (var contents in response.transform(Utf8Decoder())) {
+        buffer.write(contents);
+      }
 
       if (_config.successStatusCode.length > 0 &&_config.successStatusCode.indexOf(response.statusCode) > -1) {
         ResultModel model = ResultModel();
@@ -269,23 +271,10 @@ class GenericRequestObject<RequestType extends Serializable, ResponseType extend
         }
 
         try {
-          model.result = utf8.decode(bytes);
-          if (_isParse) {
-            await request.done;
-            return _learning.checkSuccess<ResponseType>(_listener, model);
-          }
-
-          //buffer.write(String.fromCharCodes(bytes));
-          await for (var contents in response.transform(Utf8Decoder())) {
-            buffer.write(contents);
-          }
 
           if (buffer.isNotEmpty) {
             var body = json.decode(model.result);
 
-            if (_cache != null && _cache.options.enabled) {
-              _cache.save(bytes: bytes, duration: _cache.options.duration);
-            }
 
             if (!_asList) {
               var map = json.decode(buffer.toString());
@@ -308,8 +297,8 @@ class GenericRequestObject<RequestType extends Serializable, ResponseType extend
             }
           }
         } catch (e) {
-          String s = String.fromCharCodes(bytes);
-          model.bodyBytes = new Uint8List.fromList(s.codeUnits);
+          //String s = String.fromCharCodes(bytes);
+         // model.bodyBytes = new Uint8List.fromList(s.codeUnits);
           model.result = "";
           print(e);
         }
@@ -322,7 +311,7 @@ class GenericRequestObject<RequestType extends Serializable, ResponseType extend
           return model;
         }
       } else {
-        buffer.write(String.fromCharCodes(bytes));
+        //buffer.write(String.fromCharCodes(bytes));
         await request.done;
         ErrorModel error = ErrorModel();
         error.description = response.reasonPhrase;
